6ab951fdfd6b302958f59b774514cca75312c969
2005869 <2005869@aluno.univesp.br>
2022-12-20 15:42:27 -0300

    put first letter in upper case

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,34 @@
 <template>
   <div id="app">
-
+    <div v-for="(poke, index) in pokemons" :key="index">
+      <PokemonView :name="poke.name" :url="poke.url" :num="index + 1"/>
+    </div>
   </div>
 </template>
 
 <script>
-//import axios from 'axios';
-
+import axios from 'axios';
+import PokemonView from './components/PokemonView.vue'
 export default {
   name: 'App',
+
+  data() {
+    return {
+      pokemons: []
+    }
+  },
+
+  created: function(){
+    axios.get('https://pokeapi.co/api/v2/pokemon?limit=151&offset=0').then(res => {
+      console.log('Get pokemon list');
+      this.pokemons = res.data.results;
+    }).catch(err => {
+      console.log(err);
+    });
+  },
+  
   components: {
+    PokemonView,
   }
 }
 </script>
---
+++ src/components/PokemonView.vue
@@ -0,0 +1,32 @@
+<template>
+  
+  <div>
+    <h1>{{ num }} - {{ upper(name)}}</h1>
+    <small>{{ url }}</small>
+  </div>
+</template>
+
+<script>
+export default {
+    props: {
+        num: Number,
+        name: String,
+        url: String
+    },
+
+    computed: {
+      
+    },
+
+    methods: {
+      upper: function(value){
+        var newName = value[0].toUpperCase() + value.slice(1);
+        return newName;
+      }
+    }
+}
+</script>
+
+<style>
+
+</style>
